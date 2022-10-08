5edbcae1e60bfc8beb6dfb0134bd1847d3b87f33
Александр Брюханов <alexbruyhanov@yandex.ru>
2022-10-08 19:34:09 +0700

    ДЗ_Python_4

---
+++ ДЗ4/Задача4.py
@@ -0,0 +1,19 @@
+# 4) Определить, позицию второго вхождения строки в списке либо сообщить, что её нет.
+
+test_list = []
+print(f"список: {test_list}")
+test_item = input("Введите искомую строку: ")
+
+
+def check_list(test_list, test_item):
+    count = 0
+    for i in range(len(test_list)):
+        if test_list[i] == test_item:
+            count += 1
+            if count == 2:
+                return i
+    else:
+        return -1
+
+
+print(f"ответ: {check_list(test_list, test_item)}")
